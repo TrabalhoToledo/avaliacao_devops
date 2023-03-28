a04ca118744d2644abb389c37af083965b0a2dfa
discollizard <discollizard@github.com>
2023-03-28 15:07:42 -0700

    finished cpt8

---
+++ README.md
@@ -0,0 +1,4 @@
+# Avaliação DevOps - Álvaro Ascenço
+
+Esse é um programa simples feito em C pra encontrar o número mais alto dentre os passados pelo terminal, este arquivo readme
+serve também como um artefato de documentação.
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int max = atoi(argv[1]);
+    for (int i = 2; i < argc; i++) {
+        int num = atoi(argv[i]);
+        if (num > max) {
+            max = num;
+        }
+    }
+    printf("The highest number is: %d\n", max);
+    return 0;
+}
